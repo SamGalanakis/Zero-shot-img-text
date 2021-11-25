ce232420011095713323a300f072341f8d208db7
Samuel Galanakis <samme.galanakis6@gmail.com>
2021-11-25 02:07:49 +0100

    Remove extra folder

--- voxelizer_extension/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, Extension
-from torch.utils import cpp_extension
-
-setup(name='voxelizer_cpp',
-      ext_modules=[cpp_extension.CppExtension('voxelizer_cpp', ['voxelizer.cpp'])],
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
-
